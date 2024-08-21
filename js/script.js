164f3890f3eb7dbb68a61c2c3386a0ee9c965da4
taaats74 <taaats74@gmail.com>
2024-08-22 06:26:21 +0900

    working

---
+++ js/script.js
@@ -0,0 +1,64 @@
+"use strict"
+
+{
+  $(document).ready(function(){
+    $('.voice-wrapper').slick({
+    autoplay: true,
+    speed: 1500,
+    autplaySpeed: 3000,
+    dots: true,
+    nextArrow: $(".next"),
+    prevArrow: $(".prev"),
+    slidesToShow: 3,
+      responsive: [
+        {
+          breakpoint: 880,
+          settings: {
+            slidesToShow: 1,
+          }
+        },
+      ]
+    });
+  });
+
+  window.addEventListener('load', function() {
+  var maxHeight = 0;
+  $('.voice-wrapper .voice .inner').each(function(idx, elem) {
+    var height = $(elem).height();
+    if(maxHeight < height) {
+      maxHeight = height;
+    }
+  });
+    $('.voice-wrapper .voice .inner').height(maxHeight);
+  });
+}
+
+{
+  $(function(){
+    $('.faq-slide').slick({
+      autoplay: true, //自動でスクロール
+      autoplaySpeed: 0, //自動再生のスライド切り替えまでの時間を設定
+      speed: 8000, //スライドが流れる速度を設定
+      cssEase: "linear", //スライドの流れ方を等速に設定
+      slidesToShow: 4, //表示するスライドの数
+      swipe: false, // 操作による切り替えはさせない
+      arrows: false, //矢印非表示
+      pauseOnFocus: false, //スライダーをフォーカスした時にスライドを停止させるか
+      pauseOnHover: false, //スライダーにマウスホバーした時にスライドを停止させるか
+      responsive: [
+        {
+          breakpoint: 820,
+          settings: {
+            slidesToShow: 3, //画面幅750px以下でスライド3枚表示
+          }
+        },
+        {
+          breakpoint: 500,
+          settings: {
+            slidesToShow: 2, //画面幅750px以下でスライド3枚表示
+          }
+        }
+      ]
+    });
+  });
+}
